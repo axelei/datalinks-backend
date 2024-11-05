5bf1e1ff563a7d217c07436cca4e830400e58469
Krusher <axelei@gmail.com>
2024-11-05 08:49:45 +0100

    Captcha fix

--- web-layer/src/main/java/net/krusher/datalinks/common/CaptchaHelper.java
+++ web-layer/src/main/java/net/krusher/datalinks/common/CaptchaHelper.java
@@ -4,16 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Setter;
 import net.krusher.datalinks.exception.EngineException;
 import net.krusher.datalinks.exception.ErrorType;
-import net.krusher.datalinks.model.ReCaptchaModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @ConfigurationProperties(prefix = "google.recaptcha.key")
@@ -23,29 +23,18 @@ public class CaptchaHelper {
     private String site;
     private String secret;
 
-    private final ObjectMapper objectMapper;
-
-    private final String SERVICE_URL = "https://www.google.com/recaptcha/api/siteverify";
-
-    @Autowired
-    public CaptchaHelper(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+    private final String SERVICE_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s&remoteip=%s";
 
     public boolean checkCaptcha(String response, String remoteIP) {
-        try (HttpClient client = HttpClient.newHttpClient()) {
-            ReCaptchaModel reCaptchaModel = ReCaptchaModel.builder()
-                    .response(response)
-                    .remoteIp(remoteIP)
-                    .secret(secret)
-                    .build();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(SERVICE_URL))
-                    .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(reCaptchaModel)))
-                    .build();
-            HttpResponse<String> reCaptchaResponse = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return reCaptchaResponse.body().contains("\"success\": true");
-        } catch (InterruptedException | IOException e) {
+        try {
+            URL url = new URI(String.format(
+                    SERVICE_URL,
+                    secret, response, remoteIP)).toURL();
+            try (InputStream in = url.openStream()) {
+                String googleResponse = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+                return googleResponse.contains("\"success\": true");
+            }
+        } catch (IOException | URISyntaxException e) {
             throw new EngineException(ErrorType.CAPTCHA_ERROR, e);
         }
     }
