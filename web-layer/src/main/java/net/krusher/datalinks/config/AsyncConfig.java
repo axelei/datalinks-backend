d2c91224bac44d6183cf8cba19859005f2f4363a
Krusher <axelei@gmail.com>
2024-11-22 23:44:29 +0100

    Async config

---
+++ web-layer/src/main/java/net/krusher/datalinks/config/AsyncConfig.java
@@ -0,0 +1,20 @@
+package net.krusher.datalinks.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "linkExecutor")
+    public ThreadPoolTaskExecutor singleThreadExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(1);
+        executor.setQueueCapacity(10);
+        executor.setThreadNamePrefix("LinkTask-");
+        executor.initialize();
+        return executor;
+    }
+}
--- web-layer/src/main/java/net/krusher/datalinks/tasks/LinkTasks.java
+++ web-layer/src/main/java/net/krusher/datalinks/tasks/LinkTasks.java
@@ -3,6 +3,7 @@ package net.krusher.datalinks.tasks;
 import net.krusher.datalinks.handler.tasks.LinkerCommandHandler;
 import net.krusher.datalinks.handler.tasks.UnlinkerCommandHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -18,11 +19,13 @@ public class LinkTasks {
         this.unlinkerCommandHandler = unlinkerCommandHandler;
     }
 
+    @Async("linkExecutor")
     @Scheduled(cron = "0 0 5 * * *")
     public void linkerTask() {
         linkerCommandHandler.handler();
     }
 
+    @Async("linkExecutor")
     @Scheduled(cron = "0 0 7 * * *")
     public void unlinkerTask() {
         unlinkerCommandHandler.handler();
