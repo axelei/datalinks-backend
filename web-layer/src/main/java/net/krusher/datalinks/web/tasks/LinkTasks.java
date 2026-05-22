e21b9f6edecd0372bc921034d91608361d7b31e1
Krusher <axelei@gmail.com>
2026-05-22 09:08:28 +0200

    Proper context on scheduled tasks

--- web-layer/src/main/java/net/krusher/datalinks/web/tasks/LinkTasks.java
+++ web-layer/src/main/java/net/krusher/datalinks/web/tasks/LinkTasks.java
@@ -2,6 +2,7 @@ package net.krusher.datalinks.web.tasks;
 
 import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import net.krusher.datalinks.application.handler.tasks.LinkerCommandHandler;
 import net.krusher.datalinks.application.handler.tasks.UnlinkerCommandHandler;
@@ -18,11 +19,13 @@ public class LinkTasks {
         this.unlinkerCommandHandler = unlinkerCommandHandler;
     }
 
+    @ActivateRequestContext
     @Scheduled(cron = "0 0 5 * * ?", concurrentExecution = Scheduled.ConcurrentExecution.SKIP)
     public void linkerTask() {
         linkerCommandHandler.handler();
     }
 
+    @ActivateRequestContext
     @Scheduled(cron = "0 0 7 * * ?", concurrentExecution = Scheduled.ConcurrentExecution.SKIP)
     public void unlinkerTask() {
         unlinkerCommandHandler.handler();
--- web-layer/src/main/java/net/krusher/datalinks/web/tasks/TokenTasks.java
+++ web-layer/src/main/java/net/krusher/datalinks/web/tasks/TokenTasks.java
@@ -2,6 +2,7 @@ package net.krusher.datalinks.web.tasks;
 
 import io.quarkus.scheduler.Scheduled;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
 import net.krusher.datalinks.application.handler.tasks.TokenCleanupCommandHandler;
 
@@ -15,6 +16,7 @@ public class TokenTasks {
         this.tokenCleanupCommandHandler = tokenCleanupCommandHandler;
     }
 
+    @ActivateRequestContext
     @Scheduled(cron = "0 0 4 * * ?")
     public void tokenCleanup() {
         tokenCleanupCommandHandler.handler();
